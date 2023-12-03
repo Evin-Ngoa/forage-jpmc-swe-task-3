4bebb5964eeaa3547a963be8b591009cb94e4ae4
Evin-Ngoa <toneevin07@gmail.com>
2023-12-03 11:07:05 +0300

    Finish task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,48 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  /**
+   * This function calculates the current ratio and compare it to the predetermined bounds. 
+   * This is where the logic resides to trigger alerts if the ratio crosses these bounds.
+   * @param serverRespond 
+   * @returns 
+   */
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    // Calculate the average price for stock ABC by averaging the bid and ask prices.
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    // Calculate the average price for stock DEF by averaging the bid and ask prices.
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    // Calculate the ratio of stock ABC's price to stock DEF's price.
+    const ratio = priceABC / priceDEF;
+    // Define the upper bound as 5% above 1.
+    // Tested with 0.06, 0.07 but 0.05 had the best alerting behavior.
+    const upperBound = 1 + 0.05;
+    // Define the lower bound as 5% below 1.
+    const lowerBound = 1 - 0.05;
+    // Return a new row object containing the computed values and a timestamp.
+    return {
+      price_abc: priceABC,       // The average price of stock ABC.
+      price_def: priceDEF,       // The average price of stock DEF.
+      ratio,                     // The ratio of the two stock prices.
+      // The most recent timestamp between the two stocks.
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,   // The upper bound value.
+      lower_bound: lowerBound,   // The lower bound value.
+      // If the ratio exceeds the upper or lower bounds, set trigger_alert to the ratio value; otherwise, it's undefined.
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
+  
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -18,40 +18,82 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
+  /**
+   * componentDidMount: runs after the component output has been rendered to the DOM.
+   * Here, graph's initial state is se up, 
+   * defining how it should process and display the incoming data, 
+   * ensuring the ratio and bounds are clearly visualized.
+   */
   componentDidMount() {
-    // Get element from the DOM.
+    // This gets the 'perspective-viewer' element from the DOM, which is where our graph will be displayed.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
+    // Here, we define the structure of the data that our graph will use, specifying the type of each piece of data.
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float', // This is the price for stock ABC
+      price_def: 'float', // This is the price for stock DEF
+      upper_bound: 'float', // This is the pre-calculated upper bound value
+      lower_bound: 'float', // This is the pre-calculated lower bound value
+      trigger_alert: 'float', // This will indicate when our ratio crosses the bounds
+      ratio: 'float', // The ratio of the prices of stock ABC to stock DEF using `price_abc` & `price_def` respectively
+      timestamp: 'date', // The time at which these values were recorded
     };
 
+    // Check if the 'perspective' library is available and create a table with our schema.
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+
+    // If the table was created successfully, we configure the 'perspective-viewer' element.
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('view', 'y_line'); // Sets the graph to display data as a line graph
+      elem.setAttribute('row-pivots', '["timestamp"]'); // Sets 'timestamp' as the x-axis.
+      
+      /**
+       * The 'columns' attribute specifies which data fields should be plotted along the y-axis of our graph.
+       * This helps to eliminate any irrelevant data that would otherwise clutter the visualization.
+       * By setting this attribute to include only 'ratio', 'lower_bound', 'upper_bound', and 'trigger_alert',
+       * we ensure that the graph displays only the most pertinent information that traders need to monitor.
+       */
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); // Chooses which columns to display in the graph.
+
+
+      /**
+       * Configures how the data should be aggregated or summarized.
+       * The 'aggregates' attribute helps us manage duplicate data points by merging them into a single entry.
+       * This is particularly useful for consolidating data that appears similar but may have been logged multiple times.
+       * By configuring the attribute, we treat a data point as unique only by its timestamp,
+       * while averaging the values of other shared fields like 'price_abc' and 'upper_bound'.
+       * This approach ensures our graph reflects a clean and accurate representation of the data over time.
+       */
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
       }));
     }
   }
 
+  /**
+   * The 'componentDidUpdate' method is a lifecycle hook that runs after the component has been updated.
+   */
   componentDidUpdate() {
+    /**
+     * This typically happens when new data is received and needs to be reflected in the graph.
+     * Here, we make an essential update to the way we handle this new data.
+     * By updating the argument passed to this.table.update, we ensure that the graph processes and displays
+     * the most recent data points accurately, keeping the visualization current and meaningful for the user.
+     */
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
+      ] as unknown as TableData
       );
     }
   }
